e90636d1054b261ffc5e0860834760d83830b3d9
rounakbhowmick <rounakchakda@gmail.com>
2021-01-11 16:07:01 +0530

    cookie set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cookies';
+  cookieset = false;
+  constructor(private cookie: CookieService) {}
+  clicked() {
+    this.cookie.set('Mfilterit', 'Rounak');
+    this.cookieset = true;
+  }
 }
